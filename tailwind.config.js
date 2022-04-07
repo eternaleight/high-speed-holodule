10bfc9d8979ef75b11129121f7fca491e2564cc6
eternaleight <eternaleight0@gmail.com>
2022-04-07 18:53:52 +0900

    holodule

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,23 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      'max-xl': {'max': '1279px'},
+      'max-lg': {'max': '1023px'},
+      'max-md': {'max': '767px'},
+      'max-sm': {'max': '639px'},
+    },
     extend: {},
   },
   plugins: [],
