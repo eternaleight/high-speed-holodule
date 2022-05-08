3171240723002c7f5b7dcfaabbc0fded523eb5d1
eternaleight <eternaleight0@gmail.com>
2022-05-08 09:07:22 +0900

    feat(head): favicon setting

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -6,6 +6,7 @@ export const HeadMeta = () => {
       <title>HighSpeed-holodule</title>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta name='description' content='' />
+        <link rel="icon" href="/favicon.ico" />
     </Head>
   )
 }
