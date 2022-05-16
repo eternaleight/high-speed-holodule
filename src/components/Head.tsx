bf469f7c1bd202b626dde77f14e7eb38c1a63cd4
eternaleight <eternaleight0@gmail.com>
2022-05-16 17:50:02 +0900

    fix(title): change name

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export const HeadMeta = () => {
   return (
     <Head>
-      <title>HighSpeed-holodule</title>
+      <title>HighSpeed holodule</title>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta name='description' content='' />
         <link rel="icon" href="/favicon.ico" />
